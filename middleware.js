e7a4248786ac22d67a16c0275d84f143c6b29368
sahilharkhani56 <harkhani5600@gmail.com>
2024-06-22 10:30:54 +0530

    update middle ware

--- middleware.js
+++ middleware.js
@@ -1,25 +1,24 @@
 // import data from './config.js';
-import jwt from 'jsonwebtoken';
-export default function authMiddleware(req,res,next){
-    const authHeader=req.headers.authorization;
-    // console.log(authHeader);
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(403).json({
-            message:"Invalid auth 1"
-        })
+import jwt from "jsonwebtoken";
+export default function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({
+      message: "Invalid auth 12",
+      token: authHeader,
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.userId) {
+      req.userId = decoded.userId;
+      next();
+    } else {
+      return res.status(403).json({ message: "Invalid auth 2" });
     }
-    const token=authHeader.split(' ')[1];
-    try{
-        const decoded=jwt.verify(token,data.JWT_SECRET);
-        if(decoded.userId){
-            req.userId=decoded.userId;
-            next();
-        }
-        else{
-        return res.status(403).json({message:"Invalid auth 2"});
-        }
-
-    }catch(error){
-        return res.status(403).json({message:"Invalid auth 3" });
-    }
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid auth 3" });
+  }
 }
